5122e3db074e51bbde84432fdfbbd666d9d85424
Ariel Schmeing <ariel.schmeing@proton.me>
2025-09-02 23:40:00 -0300

    [Fixed] Resolved API dockerfile

--- api/src/test/java/br/com/armardur/api/ArmardurApplicationTests.java
+++ api/src/test/java/br/com/armardur/api/ArmardurApplicationTests.java
@@ -1,13 +1,10 @@
 package br.com.armardur.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ArmardurApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
